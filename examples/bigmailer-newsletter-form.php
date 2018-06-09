68b92fb6b4833209a99d7becf6e9f893dd6c8620
Adrina <adrian@registros.net>
2018-06-09 17:39:20 +0200

    Updated of documents.

--- examples/bigmailer-newsletter-form.php
+++ examples/bigmailer-newsletter-form.php
@@ -23,7 +23,7 @@
 		use Registros\Bigmailer\Newsletter\Form;
 
 		$newsletter = new Form('9369e4a05bf1a739aa91d682fca8c905bb7af03b');
-//		$newsletter->setCss('examples/bigmailer-newsletter-form.css');
+		$newsletter->setCss('examples/bigmailer-newsletter-form.css');
 		$newsletter->setCallback('examples/bigmailer-newsletter-callback.php');
 //		$newsletter->setTitle('Newsletter');
 		
